a72d37fd332119cc54246fe6ec875f8b7e67e81f
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-10-26 13:03:32 -0300

    configurando o knex e usando o zod

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 
 export function errorHandling(error: any, request: Request, response: Response, _:NextFunction){
     // Erro lançado por nos
@@ -7,6 +8,11 @@ export function errorHandling(error: any, request: Request, response: Response, 
         return response.status(error.statusCode).json({ message: error.message })
     }
 
-    // Erros genericos
+    if(error instanceof ZodError){
+        return response.status(400)
+        .json({ message: "validation error", issues: error.format() })
+    }
+
+    // Erros genericos da propia aplicacao
     return response.status(500).json({ message: error.message})
 }
