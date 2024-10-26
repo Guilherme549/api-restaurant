e4d9744f90d18ca46df5a6284f57547032e55ce4
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-10-26 16:39:31 -0300

    abrindo a mesa

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex";
 import { z } from "zod"
 
 class TableSessionsController {
@@ -7,8 +8,14 @@ class TableSessionsController {
             const bodySchema = z.object({
                 table_id: z.number(),
             })
-            
+
             const { table_id } = bodySchema.parse(request.body)
+
+            await knex<TableSessionsRepository>("tables_sessions").insert({
+                table_id,
+                opened_at: knex.fn.now(),
+            })
+
             return response.status(201).json()
         } catch (error) {
             next(error)
