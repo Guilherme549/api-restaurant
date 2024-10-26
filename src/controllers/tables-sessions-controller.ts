25e8a37a28fd357432866aeead7ee828328e523b
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-10-26 16:05:52 -0300

    Criando rotas e controllers de table session

---
+++ src/controllers/tables-sessions-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod"
+
+class TableSessionsController {
+    async create(request: Request, response: Response, next: NextFunction){
+        try {
+            const bodySchema = z.object({
+                table_id: z.number(),
+            })
+            
+            const { table_id } = bodySchema.parse(request.body)
+            return response.status(201).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+    
+}
+
+export { TableSessionsController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
+
 import { productsRoutes } from "./products-routes";
 import { tableRoutes } from "./tables-routes";
+import { tableSessionsRoutes } from "./tables-sessions-routes";
 
 const routes = Router()
 
+routes.use("/tables-sessions", tableSessionsRoutes)
 routes.use("/products", productsRoutes)
 routes.use("/tables", tableRoutes)
+
 
 export { routes }
